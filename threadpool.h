49c7bd52e34c477e8118b28d9f166baeaf778c28
junsuyiji <83826026+junsuyiji@users.noreply.github.com>
2023-11-24 16:22:38 +0800

    Update threadpool.h

--- threadpool.h
+++ threadpool.h
@@ -9,229 +9,226 @@
 #include <condition_variable>
 #include <functional>
 #include <unordered_map>
+#include <thread>
 
+// Any类型：可以接收任意数据的类型
+class Any
+{
+public:
+	Any() = default;
+	~Any() = default;
+	Any(const Any&) = delete;
+	Any& operator=(const Any&) = delete;
+	Any(Any&&) = default;
+	Any& operator=(Any&&) = default;
 
-//线程池支持的模式
-enum class PoolMode {
-  
-  MODE_FIXED,     //固定线程数量
-  MODE_CACHED,    //线程数量可以动态增长
+	// 这个构造函数可以让Any类型接收任意其它的数据
+	template<typename T>  // T:int    Derive<int>
+	Any(T data) : base_(std::make_unique<Derive<T>>(data))
+	{}
+
+	// 这个方法能把Any对象里面存储的data数据提取出来
+	template<typename T>
+	T cast_()
+	{
+		Derive<T>* pd = dynamic_cast<Derive<T>*>(base_.get());
+		if (pd == nullptr)
+		{
+			throw "type is unmatch!";
+		}
+		return pd->data_;
+	}
+private:
+	// 基类类型
+	class Base
+	{
+	public:
+		virtual ~Base() = default;
+	};
+
+	// 派生类类型
+	template<typename T>
+	class Derive : public Base
+	{
+	public:
+		Derive(T data) : data_(data) 
+		{}
+		T data_;  // 保存了任意的其它类型
+	};
+
+private:
+	// 定义一个基类的指针
+	std::unique_ptr<Base> base_;
 };
 
-//接受任意类型的基类
-class Any {
-  public:
-    Any() = default;
-    ~Any() = default;
-    Any(const Any&) = delete;
-    Any& operator = (const  Any&) = delete;
-    Any(Any&&) = default;
-    Any& operator = (Any&&) = default;
-
-    template<typename T>
-    Any(T data) : base_(std::make_unique<Derive<T>>(data))
-    {}
-
-    template<typename T> 
-    T cast_()
-    {
-      Derive<T>* pd = dynamic_cast<Derive<T>*>(base_.get());
-
-      if (pd == nullptr)
-      {
-        throw "type is unmatch";
-      }
-
-      return pd->data_;
-    }
-  private:
-    //基类类型
-    class Base 
-    {
-      public:
-        virtual ~Base() = default;
-    };
-
-    //派生类型
-    template<typename T>
-    class Derive : public Base
-    {
-      public:
-        Derive(T data) : data_(data)
-        {}
-      T data_;
-      private:
-        
-    };
-
-  private:
-    std::unique_ptr<Base> base_;
-
-};
-
-//实现一个信号量的类
+// 实现一个信号量类
 class Semaphore
 {
-  public:
-    Semaphore(int limit = 0)
-      : resLimit_(limit)
-    {}
-    ~Semaphore() = default;
+public:
+	Semaphore(int limit = 0) 
+		:resLimit_(limit)
+	{}
+	~Semaphore() = default;
 
-    //获取一个信号量资源
-    void wait()
-    {
-      std::unique_lock<std::mutex> lock(mtx_);
-      cond_.wait(lock, [&]()->bool {return resLimit_ > 0;});
-      resLimit_--;
-    }
+	// 获取一个信号量资源
+	void wait()
+	{
+		std::unique_lock<std::mutex> lock(mtx_);
+		// 等待信号量有资源，没有资源的话，会阻塞当前线程
+		cond_.wait(lock, [&]()->bool {return resLimit_ > 0; });
+		resLimit_--;
+	}
 
-    //增加一个信号量资源
-    void post()
-    {
-      std::unique_lock<std::mutex> lock(mtx_);
-      resLimit_++;
-      cond_.notify_all();
-    }
-
-
-  private:
-    int resLimit_;
-    std::mutex mtx_;
-    std::condition_variable cond_;
-
-
+	// 增加一个信号量资源
+	void post()
+	{
+		std::unique_lock<std::mutex> lock(mtx_);
+		resLimit_++;
+		// linux下condition_variable的析构函数什么也没做
+		// 导致这里状态已经失效，无故阻塞
+		cond_.notify_all();  // 等待状态，释放mutex锁 通知条件变量wait的地方，可以起来干活了
+	}
+private:
+	int resLimit_;
+	std::mutex mtx_;
+	std::condition_variable cond_;
 };
 
+// Task类型的前置声明
 class Task;
 
-//实现接受线程池task任务完成之后的返回值雷兴国result
+// 实现接收提交到线程池的task任务执行完成后的返回值类型Result
 class Result
 {
-  public:
-    Result(std::shared_ptr<Task> task, bool isValid = true);
-    ~Result() = default;
+public:
+	Result(std::shared_ptr<Task> task, bool isValid = true);
+	~Result() = default;
 
-    void setVal(Any any);
-    Any get();
-  private:
-    Any any_;
-    Semaphore sem_;
-    std::shared_ptr<Task> task_;
-    std::atomic_bool isValid_;
+	// setVal方法，获取任务执行完的返回值的
+	void setVal(Any any);
+
+	// get方法，用户调用这个方法获取task的返回值
+	Any get();
+private:
+	Any any_; // 存储任务的返回值
+	Semaphore sem_; // 线程通信信号量
+	std::shared_ptr<Task> task_; //指向对应获取返回值的任务对象 
+	std::atomic_bool isValid_; // 返回值是否有效
 };
 
+// 任务抽象基类
+class Task
+{
+public:
+	Task();
+	~Task() = default;
+	void exec();
+	void setResult(Result* res);
 
-//任务抽象基类
-class Task {
-  public:
+	// 用户可以自定义任意任务类型，从Task继承，重写run方法，实现自定义任务处理
+	virtual Any run() = 0;
 
-    Task();
-    ~Task() = default;
-
-    void exec();
-    void setResult(Result *res);
-    //用户可以自定义任务类型，从Task继承，重写run方法，实现自定义任务处理
-    virtual Any run() = 0;
-
-  private:
-    Result* result_;
+private:
+	Result* result_; // Result对象的声明周期 》 Task的
 };
 
-//线程类型
-class Thread {
+// 线程池支持的模式
+enum class PoolMode
+{
+	MODE_FIXED,  // 固定数量的线程
+	MODE_CACHED, // 线程数量可动态增长
+};
 
-  public:
-    //线程函数对象类型
-    using ThreadFunc = std::function<void(int)>;
-    //线程构造
-    Thread(ThreadFunc func);
-    //线程析构
-    ~Thread();
+// 线程类型
+class Thread
+{
+public:
+	// 线程函数对象类型
+	using ThreadFunc = std::function<void(int)>;
 
-    void start();
+	// 线程构造
+	Thread(ThreadFunc func);
+	// 线程析构
+	~Thread();
+	// 启动线程
+	void start();
 
-    //获取线程id
-    int getId() const;
-  private:
-    ThreadFunc func_;
-    static int generateId_;
-    int threadId_; //保存线程id
+	// 获取线程id
+	int getId()const;
+private:
+	ThreadFunc func_;
+	static int generateId_;
+	int threadId_;  // 保存线程id
 };
 
 /*
-example 
+example:
 ThreadPool pool;
 pool.start(4);
 
 class MyTask : public Task
 {
-  public:
-    void run() {code};
-
+	public:
+		void run() { // 线程代码... }
 };
 
 pool.submitTask(std::make_shared<MyTask>());
-
 */
+// 线程池类型
+class ThreadPool
+{
+public:
+	// 线程池构造
+	ThreadPool();
 
-//线程池类型
-class ThreadPool {
+	// 线程池析构
+	~ThreadPool();
 
-  public:
-    ThreadPool();
-    ~ThreadPool();
+	// 设置线程池的工作模式
+	void setMode(PoolMode mode);
 
-    //设置工作模式
-    void setMode(PoolMode mode);
+	// 设置task任务队列上线阈值
+	void setTaskQueMaxThreshHold(int threshhold);
 
-    //开启线程池
-    void start(int initThreadSize = 4);
+	// 设置线程池cached模式下线程阈值
+	void setThreadSizeThreshHold(int threshhold);
 
-    //给任务队列设置上限阈值
-    void setTaskQueMaxThreshHold(int threshhold);
+	// 给线程池提交任务
+	Result submitTask(std::shared_ptr<Task> sp);
 
-    //设置初始线程数量
-    void setInitThreadSize(int size); 
-    
-    //给线程池提交任务
-    Result submitTask(std::shared_ptr<Task> sp);
+	// 开启线程池
+	void start(int initThreadSize = std::thread::hardware_concurrency());
 
-    ThreadPool(const ThreadPool&) = delete;
-    ThreadPool& operator = (const ThreadPool&) = delete;
+	ThreadPool(const ThreadPool&) = delete;
+	ThreadPool& operator=(const ThreadPool&) = delete;
 
-    //设置cached模式下线程的阈值
-    void setThreadSizeThreshHold(int threshhold);
+private:
+	// 定义线程函数
+	void threadFunc(int threadid);
 
-  private:
-  //定义线程执行函数
-  void threadFunc(int threadid);
+	// 检查pool的运行状态
+	bool checkRunningState() const;
 
-  //检查pool的运行装填
-  bool checkRunningState() const;
+private:
+	// std::vector<std::unique_ptr<Thread>> threads_; // 线程列表
+	std::unordered_map<int, std::unique_ptr<Thread>> threads_; // 线程列表
 
-  private:
-    //std::vector<std::unique_ptr<Thread>> threads_; //线程列表
-    std::unordered_map<int, std::unique_ptr<Thread>> threads_;
-    int initThreadSize_;           //初始线程数量
-    std::atomic_int curThreadSize_;  //记录当前线程池里面的总数量
-    int threadSizeThreshHold_;   //cached模式下，线程上限的阈值
-    std::queue<std::shared_ptr<Task>> taskQue_; //任务队列
-    std::atomic_int taskSize_; //任务数量
-    int taskQueMaxThreshHold_; //任务队列数量上限
+	int initThreadSize_;  // 初始的线程数量
+	int threadSizeThreshHold_; // 线程数量上限阈值
+	std::atomic_int curThreadSize_;	// 记录当前线程池里面线程的总数量
+	std::atomic_int idleThreadSize_; // 记录空闲线程的数量
 
-    std::mutex taskQueMtx_;        //保证任务队列线程安全
-    std::condition_variable notFull_;  //保证任务队列不满
-    std::condition_variable notEmpty_; //保证任务队列不空
+	std::queue<std::shared_ptr<Task>> taskQue_; // 任务队列
+	std::atomic_int taskSize_; // 任务的数量
+	int taskQueMaxThreshHold_;  // 任务队列数量上限阈值
 
-    PoolMode poolMode_;  //当前线程池的工作模式
+	std::mutex taskQueMtx_; // 保证任务队列的线程安全
+	std::condition_variable notFull_; // 表示任务队列不满
+	std::condition_variable notEmpty_; // 表示任务队列不空
+	std::condition_variable exitCond_; // 等到线程资源全部回收
 
-    //当前线程池的启动状态
-    std::atomic_bool isPoolRunning_;
-
-    //记录空闲线程数量
-    std::atomic_int idleThreadSize_;
+	PoolMode poolMode_; // 当前线程池的工作模式
+	std::atomic_bool isPoolRunning_; // 表示当前线程池的启动状态
 };
-
 
 #endif
